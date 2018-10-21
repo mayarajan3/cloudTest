444314f9e30509cb2dbc6314df7d6fe0b7873e67
mayarajan3 <mayarajan3@yahoo.com>
2018-10-21 03:00:35 -0400

    Fixing paths

--- Canopy/routes/index.js
+++ Canopy/routes/index.js
@@ -15,14 +15,14 @@ function decode(data) {
 
 function callExec(req, res){
   console.log("About to log image..");
-  console.log(req.body.image);
   var dataText = req.body.image;
   console.log("callExec() start");
-
   dataText = dataText.split(",")[1];
   console.log(__dirname);
   fs.writeFileSync(__dirname+'/image.bananas', dataText, { flag: 'w'});
-  exec('bash ' + __dirname+ '/output.exe -p '+ __dirname+'/image.bananas', function(err, data) {
+  var code = 'bash cd '+ __dirname+' && ' + __dirname+ '/output.exe -p '+ __dirname+'/image.bananas -d ' + __dirname;
+  exec(code, function(err, data) {
+       console.log(code);
        console.log('data: /n' + data);
        console.log('error: /n' + err);
        res.json(data);
