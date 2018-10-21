002d6b9fea535a0d3afefff1c0ebee40dc7e8138
mayarajan3 <mayarajan3@yahoo.com>
2018-10-20 23:06:35 -0400

    Working backend

--- Canopy/routes/index.js
+++ Canopy/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var exec = require('child_process').execFile;
+var exec = require('child_process').exec;
 var fs = require("fs");
 
 
@@ -18,16 +18,19 @@ function callExec(req, res){
   console.log(req.body.image);
   var dataText = req.body.image;
   console.log("callExec() start");
-  fs.writeFileSync('image.bananas', dataText, { flag: 'w'});
-  exec('output.exe < image.bananas'+ function(err, data) {  
+
+  dataText = dataText.split(",")[1];
+  console.log(__dirname);
+  fs.writeFileSync(__dirname+'/image.bananas', dataText, { flag: 'w'});
+  exec('bash ' + __dirname+ '/output.exe -p '+ __dirname+'/image.bananas', function(err, data) {
        console.log('data: /n' + data);
        console.log('error: /n' + err);
-       res.json(data);                      
-   });  
+       res.json(data);
+   });
 }
 
 router.post('/upload', function (req, res, next) {
-  
+
   callExec(req, res);
 });
 
