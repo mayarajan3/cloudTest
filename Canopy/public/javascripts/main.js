d5c7a36f0ae2d6d8dcc47c22e3a88dd4205ad51a
aeropayman <aeropayman@gmail.com>
2018-10-21 07:30:21 -0400

    final edits

--- Canopy/public/javascripts/main.js
+++ Canopy/public/javascripts/main.js
@@ -1,10 +1,31 @@
-var imageData = '';
+var apikey = "AIzaSyD1kTgXzQV20Ab_hXTML1AVWCoETfh4iCw";
+//Specifies how much to offset each image in the grid. Calculated by finding the difference
+//between the lattitude of the edge of the map section and the center. Recalculate new offset if zoom level is changed. 
+var offset = 0.004299*2;
+//https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=12&size=400x400&key=YOUR_API_KEY
+var globalGridSize = 3;
+var imageArr=createImgArray;
+
+//var imageData = "";
 
 function clicky () {
+    displayImage($('#uploadBtn')[0]);
     encodeImageFileAsURL($('#uploadBtn')[0]);
     var file = {"image" : imageData};
     $.post("/upload", file, function(data, status){
-        $('p').html(data);
+        $('#fileUpload').html(data);
+    }, "json");
+}
+
+function clicky2 () {
+    imageArr = createImgArray($("#latCoord").val(), $("#longCoord").val(), 16, 400, apikey);
+    $.post("/uploadCoords", {imageArr:imageArr}, function(data, status){
+        //console.log(data);
+        var dataArray = $.map(data, function (item) {
+            return item;
+        });
+        //console.log(dataArray[8]);
+        $('#testtest').html(dataArray[8]);
     }, "json");
 }
 
@@ -16,4 +37,51 @@ function encodeImageFileAsURL(element) {
     }
     reader.readAsDataURL(file);
   }
+
+  function displayImage(element) {
+
+    var file = element.files[0];
+    var reader  = new FileReader();
+    // it's onload event and you forgot (parameters)
+    reader.onload = function(e)  {
+        var image = document.createElement("img");
+        // the result image data
+        image.src = e.target.result;
+        image.style = "width:400px; height:400px; border-radius:3px;text-align:center";
+        image.class = "userImage";
+        $('.help').after(image);
+     }
+     // you have to declare the file loading
+     reader.readAsDataURL(file);
+ }
+  
+//////////// Image Grid
+
+
+function generateURI(lat,long,zoom,size,key) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?' + 'center='+ lat + ',' + long + "&zoom=" + zoom
+    + "&size=" + size + "x" + size + "&format=jpg" + "&maptype=satellite" + "&key=" + key;
+}
+
+function createImgArray(originLat, originLong, zoom, size, key) {
+    var arr = new Array();
+    for(var i=0; i<globalGridSize; i++){
+        arr[i] = new Array();
+        for(var j=0; j<globalGridSize; j++){
+            var lat = originLat - i*offset;
+            var long = originLong + j*offset;
+            arr[i][j] = generateURI(lat, long, zoom, size, key);
+        }
+    }
+    return arr;
+}
+
+$(document).ready(function(e) {
+    var arr = createImgArray(2.423850, 99.314919, 16, 400, apikey);
+    for(var i=0; i<globalGridSize; i++){
+        for(var j=0; j<globalGridSize; j++){
+            $('#testtest').append("<img src=" + arr[i][j] + " style='width:200px;margin-bottom:-2px;margin-top:-2px; !important;padding;0' >");
+        }
+    }
+});
 
--- Canopy/routes/index.js
+++ Canopy/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var exec = require('child_process').exec;
+var http = require('https');
 var fs = require("fs");
 
 
@@ -9,9 +10,7 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-function decode(data) {
-
-}
+function decode(data) { }
 
 function callExec(req, res){
   console.log("About to log image..");
@@ -22,16 +21,43 @@ function callExec(req, res){
   fs.writeFileSync(__dirname+'/image.bananas', dataText, { flag: 'w'});
   var code = 'bash cd '+ __dirname+' && ' + __dirname+ '/output.exe -p '+ __dirname+'/image.bananas -d ' + __dirname;
   exec(code, function(err, data) {
-       console.log(code);
-       console.log('data: /n' + data);
-       console.log('error: /n' + err);
-       res.json(data);
-   });
+      obj = JSON.parse(data);
+      var score = obj.payload[0].classification.score;
+      var classification = obj.payload[0].displayName;
+      var output = classification + ' with ' + (score*100) + '% certainty.';
+      res.json(output);
+  });
+}
+
+function callExecArray(fileStr, res){
+  console.log("About to log image..");
+  var dataText = fileStr;
+  console.log("callExec() start");
+  dataText = dataText.split(",")[1];
+  console.log(__dirname);
+  fs.writeFileSync(__dirname+'/image.bananas', dataText, { flag: 'w'});
+  var code = 'bash cd '+ __dirname+' && ' + __dirname+ '/output.exe -p '+ __dirname+'/image.bananas -d ' + __dirname;
+  exec(code, function(err, data) {
+      obj = JSON.parse(data);
+      var classification = obj.payload[0].displayName;
+      var output = classification;
+      res.json(output);
+  });
 }
 
 router.post('/upload', function (req, res, next) {
-
   callExec(req, res);
+});
+
+router.post('/uploadCoords', function (req, res, next) {
+  var imageArrData = res.body;
+  var file = fs.createWriteStream("file.jpg");
+  var request = https.get(imageArrData[0], function(response) {
+    response.pipe(file);
+    console.log(file);
+  });
+  
+  //callExecArray(file, res);
 });
 
 module.exports = router;
